29fe5a34c969f686892976f9fe82378b913720a4
Ansley Tsai <ansley7502@gmail.com>
2020-08-01 18:37:08 -0700

    test

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+import random
+app = Flask(__name__)
+
+@app.route('/')
+
+@app.route('/home')
+def home():
+    scan=open("sentences.txt")
+    list=scan.readlines()
+    text=random.choice(list)
+    return render_template('home.html',sentence=text)
+
+@app.route('/hello')
+def hello_world():
+    return 'Hello, World'
+
+if __name__=='__main__':
+    app.run(debug=True)
